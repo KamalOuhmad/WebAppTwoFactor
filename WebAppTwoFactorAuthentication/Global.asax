7d6e37331ecf088a3321453222a8c9f82d916c42
ouhmad kamal <ouhmad.kamal@hotmail.com>
2019-05-19 17:34:42 -0400

    Initial version

---
+++ WebAppTwoFactorAuthentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppTwoFactorAuthentication.MvcApplication" Language="C#" %>
---
+++ WebAppTwoFactorAuthentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppTwoFactorAuthentication.Startup))]
+namespace WebAppTwoFactorAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
